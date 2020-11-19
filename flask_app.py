39443be4c5c675b3f88654ccc11d3eb12952fbf0
alexn11 <alexn11.gh@gmail.com>
2020-11-19 11:24:46 +0000

    cleaned main

--- flask_app.py
+++ flask_app.py
@@ -10,32 +10,18 @@ initial_state = {}
   
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def main():
 
   state = initial_state
   portfolio_columns, portfolio, tag_list = load_portfolio()
 
-  if(flask.request.method == 'GET'):
-    return flask.render_template('main.html',
-                                  state = state,
-                                  portfolio_columns = portfolio_columns,
-                                  tag_list = tag_list,
-                                  portfolio = portfolio)
+  return flask.render_template('main.html',
+                                state = state,
+                                portfolio_columns = portfolio_columns,
+                                tag_list = tag_list,
+                                portfolio = portfolio)
     
-  if(flask.request.method == 'POST'):
-  
-    form_values = flask.request.form
-    #state = {}
-    
-    return flask.render_template('main.html',
-                                 state = state,
-                                 portfolio_columns = portfolio_columns,
-                                  tag_list = tag_list,
-                                 portfolio = portfolio)
-
-
-
 if __name__ == '__main__':
     app.run()
     
